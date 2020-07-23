f1503a59e85a90230a2708795a7bba7c7d299805
isliudong <2351036454@qq.com>
2020-07-23 20:16:56 +0800

    sql-2020/7/23

--- mysql/mysql_exercise.sql
+++ mysql/mysql_exercise.sql
@@ -89,3 +89,81 @@ insert into HAND_STUDENT_CORE values ('s002','c002',72.9);
 insert into HAND_STUDENT_CORE values ('s003','c002',81.9);
 insert into HAND_STUDENT_CORE values ('s001','c003','59');
 commit;
+-- 第一单元select语句
+-- 列选择
+SELECT student_no,student_name from hand_student
+-- 行选择
+SELECT * from hand_student
+-- 多表连接查询
+SELECT hand_student.student_name,hand_student_core.course_no,hand_student_core.core
+from hand_student,hand_student_core
+
+-- 字段加减?出现多余小数
+SELECT hand_student.student_name,hand_student_core.course_no,hand_student_core.core+100
+from hand_student,hand_student_core
+-- 别名
+SELECT hand_student.student_name as 'name' ,hand_student_core.course_no 课程号,hand_student_core.core
+from hand_student,hand_student_core
+
+-- CONCAT(str1,str2,...)(N,from_base,to_base)连接
+SELECT CONCAT(hand_student.student_name,hand_student_core.course_no),hand_student_core.core
+from hand_student,hand_student_core
+
+-- 去重
+SELECT DISTINCT hand_student.student_name as 'name' ,hand_student_core.course_no 课程号,hand_student_core.core
+from hand_student,hand_student_core
+
+-- 第二单元条件限制
+SELECT hand_student.student_name from hand_student where hand_student.student_name='张三'
+
+SELECT hand_student.student_name from hand_student where hand_student.student_name<>'张三'
+
+SELECT hand_student.student_name from hand_student where hand_student.student_no>'s005'
+
+SELECT hand_student.student_name from hand_student where hand_student.student_no BETWEEN 's005' and 's006'
+
+SELECT hand_student.student_name from hand_student where hand_student.student_no in ('s005','s006')
+
+INSERT INTO hand_student VALUES('s011','王%',24,'男')
+
+SELECT hand_student.student_name FROM hand_student WHERE hand_student.student_name LIKE '王%';
+
+SELECT hand_student.student_name FROM hand_student WHERE hand_student.student_name LIKE '王.%' ESCAPE '.';
+-- 使用NULL条件：
+SELECT hand_student.student_name FROM hand_student WHERE hand_student.student_name is null;
+-- 使用多个条件组合的逻辑操作符：
+SELECT hand_student.student_name FROM hand_student WHERE hand_student.student_name LIKE '王%' and hand_student.student_no>'s010' ;
+-- 使用ORDER BY 子句进行排序：
+
+SELECT hand_student.student_name FROM hand_student WHERE hand_student.student_name LIKE '王%' ORDER BY hand_student.student_no asc ;
+
+SELECT hand_student.student_name FROM hand_student WHERE hand_student.student_name LIKE '王%' ORDER BY hand_student.student_no desc ;
+
+-- 第三单元：单行函数
+
+-- 大小写不敏感
+SELECT hand_student.student_name from hand_student where upper(hand_student.student_no)='s001'
+SELECT hand_student.student_name from hand_student where LOWER(hand_student.student_no)='S001'
+
+-- 常用字符函数
+SELECT s.student_no, LENGTH(s.student_no), INSTR(s.student_no,'0')from hand_student s where SUBSTR(s.student_no,2)='009'
+
+-- 数字操作函数：
+SELECT ROUND(45.923,2), ROUND(45.923,0), ROUND(45.923,-1) FROM DUAL;
+
+
+-- 日期操作
+SELECT SYSDATE()  FROM DUAL;
+SELECT SYSDATE()/7 weeks FROM DUAL;
+SELECT TO_DAYS(SYSDATE()) + 1 days FROM DUAL;
+SELECT DATE_ADD(SYSDATE(),INTERVAL 1 day)  FROM DUAL;
+
+
+-- 条件表达式
+
+SELECT hand_student.student_name,CASE hand_student.student_no
+	WHEN 's001' THEN
+		'ooo'
+	ELSE
+		hand_student.student_no
+END FROM hand_student;
